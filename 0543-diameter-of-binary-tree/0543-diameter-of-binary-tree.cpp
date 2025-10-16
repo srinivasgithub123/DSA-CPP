9b0bd6e77b6feee77f40691ecda3bb016c7e9667
srinivasgithub123 <srinivaskankala2004@gmail.com>
2025-10-16 21:48:03 +0530

    Time: 0 ms (100%), Space: 23.6 MB (93.05%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,36 +1,58 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+
+//     int height(TreeNode* root){
+//         if(root==nullptr)
+//         return 0;
+
+//         int ltht=height(root->left);
+//         int rtht= height(root->right);
+        
+//         return max(ltht, rtht)+1;
+//     }
+
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if(root==nullptr)
+//         return 0;
+
+//         int ltdiam= diameterOfBinaryTree(root->left);
+//         int rtdiam= diameterOfBinaryTree(root->right);
+//         int ansdiam= height(root->left)+height(root->right);
+
+//         return max(ansdiam, max(ltdiam, rtdiam));
+        
+//     }
+// };
 class Solution {
 public:
+    int maxDiameter = 0;
 
-    int height(TreeNode* root){
-        if(root==nullptr)
-        return 0;
+    int height(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
 
-        int ltht=height(root->left);
-        int rtht= height(root->right);
-        
-        return max(ltht, rtht)+1;
+        int ltht = height(root->left);
+        int rtht = height(root->right);
+
+        // Update the global diameter while computing height
+        maxDiameter = max(maxDiameter, ltht + rtht);
+
+        return max(ltht, rtht) + 1;
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root==nullptr)
-        return 0;
-
-        int ltdiam= diameterOfBinaryTree(root->left);
-        int rtdiam= diameterOfBinaryTree(root->right);
-        int ansdiam= height(root->left)+height(root->right);
-
-        return max(ansdiam, max(ltdiam, rtdiam));
-        
+        height(root);
+        return maxDiameter;
     }
 };
