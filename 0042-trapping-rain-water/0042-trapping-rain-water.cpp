3616233e08551709cc64066e9858c42ed926ecf3
srinivasgithub123 <srinivaskankala2004@gmail.com>
2025-11-05 15:39:58 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.3%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,24 +1,48 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int ans=0, n=height.size();
-        vector<int> lmax(n, 0);
-        vector<int> rmax(n, 0);
-        lmax[0]=height[0];
-        rmax[n-1]=height[n-1];
+        // int ans=0, n=height.size();
+        // vector<int> lmax(n, 0);
+        // vector<int> rmax(n, 0);
+        // lmax[0]=height[0];
+        // rmax[n-1]=height[n-1];
 
-        for(int i=1; i<n; i++)
-        lmax[i]=max(lmax[i-1], height[i]);
+        // for(int i=1; i<n; i++)
+        // lmax[i]=max(lmax[i-1], height[i]);
 
-        for(int i=n-2; i>=0; i--)
-        rmax[i]= max(rmax[i+1], height[i]);
+        // for(int i=n-2; i>=0; i--)
+        // rmax[i]= max(rmax[i+1], height[i]);
 
-        for(int i=0; i<n; i++){
-            ans+=min(lmax[i], rmax[i])-height[i];
+        // for(int i=0; i<n; i++){
+        //     ans+=min(lmax[i], rmax[i])-height[i];
+        // }
+        
+        // return ans;
+
+        int total=0, n= height.size();
+        int left= 0, right= n-1, lmax=0, rmax=0;
+
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=lmax){
+                    lmax= height[left];
+                }else{
+                    total+=lmax-height[left];
+                }
+
+                left++;
+            }else{
+                if(height[right]>=rmax){
+                    rmax= height[right];
+                }else{
+                    total+=rmax-height[right];
+                }
+
+                right--;
+            }
         }
-        
-        return ans;
-        
+
+    return total;    
         
     }
 };
