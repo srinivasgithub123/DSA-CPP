4b45e24355f8a9b2f7fac31d77896c8ef22cd59d
srinivasgithub123 <srinivaskankala2004@gmail.com>
2025-11-07 21:39:00 +0530

    Time: 0 ms (100%), Space: 59.4 MB (45.64%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.cpp
@@ -12,25 +12,39 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        TreeNode* curr= root;
-        if(root==NULL){
-            return new TreeNode(val);
-        }
-        while(root!=NULL){
-            if(val< root->val){
-                if(root->left==NULL){
-                    root->left= new TreeNode(val);
-                    break;
-                }
-                root=root->left;
-            }else if(val > root->val){
-                if(root->right==NULL){
-                    root->right= new TreeNode(val);
-                    break;
-                }
-                root= root->right;
-            }
-        }
-       return curr; 
+    //     TreeNode* curr= root;
+    //     if(root==NULL){
+    //         return new TreeNode(val);
+    //     }
+    //     while(root!=NULL){
+    //         if(val< root->val){
+    //             if(root->left==NULL){
+    //                 root->left= new TreeNode(val);
+    //                 break;
+    //             }
+    //             root=root->left;
+    //         }else if(val > root->val){
+    //             if(root->right==NULL){
+    //                 root->right= new TreeNode(val);
+    //                 break;
+    //             }
+    //             root= root->right;
+    //         }
+    //     }
+    //    return curr; 
+
+    //Recursive approach
+    //base case
+    if(root==NULL){
+        return new TreeNode(val);
+    }
+
+    if(val< root->val){
+        root->left= insertIntoBST( root->left, val);
+    }else if(val> root->val){
+        root->right= insertIntoBST(root->right, val);
+    }
+
+    return root;
     }
 };
