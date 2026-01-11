3414fc6d1ba079b00a1b1d8012eff6074f292dac
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-01-11 14:29:16 +0530

    Time: 196 ms (82.9%), Space: 88 MB (16.15%) - LeetHub

--- 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     int centeredSubarrays(vector<int>& nums) {
-        int count=0, n= nums.size();
-        for(int i=0; i<n; i++){
-            long long sum=0;
-            for(int j=i; j<n; j++){
+        // int count=0, n= nums.size();
+        // for(int i=0; i<n; i++){
+        //     long long sum=0;
+        //     for(int j=i; j<n; j++){
+        //         sum=sum+nums[j];
+        //         for(int k=i; k<=j; k++){
+        //             if(nums[k]==sum){
+        //                 count++;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+        // return count;
+
+        int count=0;
+
+        for(int i=0; i<nums.size(); i++){
+            int sum=0;
+            set<int>seen;
+            for(int j=i; j<nums.size(); j++){
                 sum=sum+nums[j];
-                for(int k=i; k<=j; k++){
-                    if(nums[k]==sum){
-                        count++;
-                        break;
-                    }
+                seen.insert(nums[j]);
+                if(seen.find(sum)!=seen.end()){
+                    count++;
                 }
             }
         }
+
         return count;
     }
 };
