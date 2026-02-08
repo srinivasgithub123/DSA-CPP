0749781b0b9dcd0eda0a88efaa8395be1cab41fc
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-02-08 13:26:48 +0530

    Time: 0 ms (100%), Space: 32.3 MB (27.4%) - LeetHub

--- 3833-count-dominant-indices/3833-count-dominant-indices.cpp
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -1,27 +1,45 @@
-class Solution {
-public:
-    int dominantIndices(vector<int>& nums) {
-        int n= nums.size();
-        int dom=0;
-        if(n<1)return 0;
-        vector<int>pref(n);
-        vector<int>prefavg(n);
-        pref[n-1]=nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            pref[i]= nums[i]+pref[i+1];
-        }
-        int c=1;
-        prefavg[n-1]= nums[n-1];
-        for(int i=n-2; i>=0; i--){
-            prefavg[i]=pref[i+1]/c;
-            c++;
-        }
-        for(int i=0; i<n; i++){
-            if(nums[i]>prefavg[i]){
-                dom++;
-            }
-        } 
-        return dom;
+// class Solution {
+// public:
+//     int dominantIndices(vector<int>& nums) {
+//         int n= nums.size();
+//         int dom=0;
+//         if(n<1)return 0;
+//         vector<int>pref(n);
+//         vector<int>prefavg(n);
+//         pref[n-1]=nums[n-1];
+//         for(int i=n-2; i>=0; i--){
+//             pref[i]= nums[i]+pref[i+1];
+//         }
+//         int c=1;
+//         prefavg[n-1]= nums[n-1];
+//         for(int i=n-2; i>=0; i--){
+//             prefavg[i]=pref[i+1]/c;
+//             c++;
+//         }
+//         for(int i=0; i<n; i++){
+//             if(nums[i]>prefavg[i]){
+//                 dom++;
+//             }
+//         } 
+//         return dom;
         
+//     }
+// };
+
+
+
+
+class Solution{
+public:
+    int dominantIndices(vector<int>& nums){
+        int n= nums.size(), sum=accumulate(nums.begin(), nums.end(), 0), cnt=0, curr=0;
+        for(int i=0; i<n; i++){
+            curr+=nums[i];
+            if(n-i-1>0 && (sum-curr)/(n-i-1)<nums[i]){
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 };
