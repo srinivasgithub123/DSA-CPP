2b283dca5fbaad8b99f55f5e31e65a70baf79b0b
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-02-08 13:14:31 +0530

    Time: 0 ms (100%), Space: 33.5 MB (5.68%) - LeetHub

---
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dominantIndices(vector<int>& nums) {
+        int n= nums.size();
+        int dom=0;
+        if(n<1)return 0;
+        vector<int>pref(n);
+        vector<int>prefavg(n);
+        pref[n-1]=nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            pref[i]= nums[i]+pref[i+1];
+        }
+        int c=1;
+        prefavg[n-1]= nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            prefavg[i]=pref[i+1]/c;
+            c++;
+        }
+        for(int i=0; i<n; i++){
+            if(nums[i]>prefavg[i]){
+                dom++;
+            }
+        } 
+        return dom;
+        
+    }
+};
