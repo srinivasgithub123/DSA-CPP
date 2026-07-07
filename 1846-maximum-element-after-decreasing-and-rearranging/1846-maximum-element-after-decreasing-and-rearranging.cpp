1cdd3c73ae2e097f13235ab720df6388abc4bd93
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-07 12:15:04 +0530

    Time: 7 ms (64.18%), Space: 55.2 MB (75.67%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,14 +1,42 @@
-class Solution {
+// class Solution {
+// public:
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         sort(arr.begin(), arr.end());
+//         arr[0]=1;
+//         for(int i=1; i<arr.size(); i++){
+//             if(abs(arr[i]-arr[i-1])>1){
+//                 arr[i]= arr[i-1]+1;
+//             }
+//         }
+//         int ans = *max_element(arr.begin(), arr.end());
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+class Solution{
 public:
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr){
         sort(arr.begin(), arr.end());
         arr[0]=1;
-        for(int i=1; i<arr.size(); i++){
-            if(abs(arr[i]-arr[i-1])>1){
-                arr[i]= arr[i-1]+1;
+            for(int i=1; i<arr.size(); i++){
+                if(abs(arr[i]-arr[i-1])>1){
+                    arr[i]=arr[i-1]+1;
+                }
             }
+        int max_elem= INT_MIN;
+        for(int i=0; i<arr.size(); i++){
+            max_elem= max(max_elem, arr[i]);
         }
-        int ans = *max_element(arr.begin(), arr.end());
-        return ans;
+
+        return max_elem;
     }
 };
