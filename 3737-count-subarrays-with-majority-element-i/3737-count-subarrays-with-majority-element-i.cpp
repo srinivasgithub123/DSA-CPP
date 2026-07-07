56b5657d5cebc413b052b3e384723bc41671b431
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-07 10:20:59 +0530

    Time: 55 ms (18.29%), Space: 39.6 MB (50.54%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,24 +1,51 @@
-class Solution {
+// class Solution {
+// public:
+//     int countMajoritySubarrays(vector<int>& nums, int target) {
+//         int result= 0;
+
+//         for(int i=0; i<nums.size(); i++){
+//             int subarray_len= 0;
+//             int count_target= 0;
+//             for(int j=i; j<nums.size(); j++){
+//                 subarray_len++;
+//                 if(nums[j]==target){
+//                     count_target+=1;
+//                 }
+
+//                 if(2*count_target> subarray_len){
+//                     result++;
+//                 }
+//             }
+
+//         }
+//     return result;    
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+class Solution{
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
-        int result= 0;
-
+    int countMajoritySubarrays(vector<int>& nums, int target){
+        int ans=0;
         for(int i=0; i<nums.size(); i++){
-            int subarray_len= 0;
-            int count_target= 0;
+            int target_count=0, size=0;
             for(int j=i; j<nums.size(); j++){
-                subarray_len++;
+                size++;
                 if(nums[j]==target){
-                    count_target+=1;
+                    target_count++;
                 }
-
-                if(2*count_target> subarray_len){
-                    result++;
+                if(target_count>size/2){
+                    ans++;
                 }
             }
-
         }
-    return result;    
-        
+    return ans;    
     }
 };
