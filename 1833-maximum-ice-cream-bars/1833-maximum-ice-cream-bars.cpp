eb8d9c95e062915c20bc228ed1a53fba05e593e8
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-07 12:59:34 +0530

    Time: 32 ms (50.07%), Space: 80.2 MB (71.26%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,19 +1,47 @@
-class Solution {
-public:
-    int maxIceCream(vector<int>& costs, int coins) {
-        vector<int>count(100001, 0);
+// class Solution {
+// public:
+//     int maxIceCream(vector<int>& costs, int coins) {
+//         vector<int>count(100001, 0);
 
-        for(int cost:costs){
-            count[cost]++;
+//         for(int cost:costs){
+//             count[cost]++;
+//         }
+//     int ans=0;
+//     for (int i = 1; i < count.size(); i++) {
+//     if (count[i] > 0 && coins >= i) {
+//         int buy = min(count[i], coins / i);
+//         ans += buy;
+//         coins -= buy * i;
+//     }
+// }
+//     return ans;  
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+class Solution{
+public:
+    int maxIceCream(vector<int>& costs, int coins){
+        int ans=0;
+        sort(costs.begin(), costs.end());
+        if(costs[0]>coins){
+            return 0;
         }
-    int ans=0;
-    for (int i = 1; i < count.size(); i++) {
-    if (count[i] > 0 && coins >= i) {
-        int buy = min(count[i], coins / i);
-        ans += buy;
-        coins -= buy * i;
+        for(int i=0; i<costs.size(); i++){
+            if(coins>=costs[i]){
+                ans++;
+                coins=coins-costs[i];
+            }
+        }
+        return ans;
     }
-}
-    return ans;  
-    }
+
 };
