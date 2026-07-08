538821b7256431e0e5725bd4dba831527e33ed74
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-08 10:21:15 +0530

    Time: 20 ms (21.89%), Space: 14.3 MB (20.51%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,56 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         unordered_set<char> charSet;
-//         int left = 0, right = 0, maxLen = 0;
+// // class Solution {
+// // public:
+// //     int lengthOfLongestSubstring(string s) {
+// //         unordered_set<char> charSet;
+// //         int left = 0, right = 0, maxLen = 0;
 
-//         while (right < s.length()) {
-//             if (charSet.find(s[right]) == charSet.end()) {
-//                 charSet.insert(s[right]);
-//                 maxLen = max(maxLen, right - left + 1);
-//                 right++;
-//             } else {
-//                 charSet.erase(s[left]);
-//                 left++;
+// //         while (right < s.length()) {
+// //             if (charSet.find(s[right]) == charSet.end()) {
+// //                 charSet.insert(s[right]);
+// //                 maxLen = max(maxLen, right - left + 1);
+// //                 right++;
+// //             } else {
+// //                 charSet.erase(s[left]);
+// //                 left++;
+// //             }
+// //         }
+
+// //         return maxLen;
+// //     }
+// // };
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+// public:
+//     int lengthOfLongestSubstring(string s){
+//         int ans=0;
+//         unordered_set<char>st;
+//         int left=0;
+
+//         for(int right=0; right<s.size(); right++){
+//             if(st.find(s[right])==st.end()){
+//                 st.insert(s[right]);
+//             }else{
+//                 while(st.count(s[right])){
+//                     st.erase(s[left]);
+//                     left++;
+//                 }
+//                 st.insert(s[right]);
 //             }
+//             ans= max(ans, right-left+1);
 //         }
 
-//         return maxLen;
+//     return ans;    
 //     }
 // };
 
@@ -33,23 +68,24 @@
 class Solution{
 public:
     int lengthOfLongestSubstring(string s){
-        int ans=0;
         unordered_set<char>st;
+        int ans= 0;
         int left=0;
-
-        for(int right=0; right<s.size(); right++){
+        int right=0;
+        while(right<s.size()){
             if(st.find(s[right])==st.end()){
                 st.insert(s[right]);
+                ans= max(ans, right-left+1);
+                right++;
             }else{
-                while(st.count(s[right])){
+                while(s[left]!=s[right]){
                     st.erase(s[left]);
                     left++;
                 }
-                st.insert(s[right]);
+                st.erase(s[left]);
+                left++;
             }
-            ans= max(ans, right-left+1);
         }
-
-    return ans;    
+        return ans;
     }
 };
