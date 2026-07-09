bd08c78b46b9f681f4235c1e328e19c790e2f7d6
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-09 08:44:04 +0530

    Time: 3 ms (65.45%), Space: 9.9 MB (89.67%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -34,30 +34,68 @@
 //     }
 //};
 
+// class Solution{
+// public:
+//     bool checkInclusion(string s1, string s2){
+//         if(s1.size()>s2.size())return false;
+//         int freq1[26]={0};
+//         int freq2[26]={0};
+//         for(int i=0; i<s1.size(); i++){
+//             freq1[s1[i]-'a']++;
+//         }
+
+//         int left=0;
+//         int right=0;
+//         while(right<s2.size() && left<=s2.size()-s1.size()+1){
+//             freq2[s2[right]-'a']++;
+//             if(right-left+1>s1.size()){
+//                 freq2[s2[left]-'a']--;
+//                 left++;
+//             }
+//             if(right-left+1==s1.size() && equal(freq1, freq1+26, freq2)){
+//                 return true;
+//             }
+//             right++;
+//         }
+
+//     return false;    
+//     }
+// };
+
+
+
+
+
+
+
+
 class Solution{
 public:
     bool checkInclusion(string s1, string s2){
-        if(s1.size()>s2.size())return false;
-        int freq1[26]={0};
-        int freq2[26]={0};
-        for(int i=0; i<s1.size(); i++){
-            freq1[s1[i]-'a']++;
+        if(s1.size()>s2.size()){
+            return false;
         }
 
-        int left=0;
-        int right=0;
-        while(right<s2.size() && left<=s2.size()-s1.size()+1){
+        int freq1[26]={0};
+        int freq2[26]={0};
+        for(char ch: s1){
+            freq1[ch-'a']++;
+        }
+
+        int left=0, right=0;
+        while(right<s2.size()){
             freq2[s2[right]-'a']++;
-            if(right-left+1>s1.size()){
-                freq2[s2[left]-'a']--;
-                left++;
-            }
             if(right-left+1==s1.size() && equal(freq1, freq1+26, freq2)){
                 return true;
             }
-            right++;
-        }
 
+            if(right-left+1==s1.size() && !equal(freq1, freq1+26, freq2)){
+                freq2[s2[left]-'a']--;
+                left++;
+
+            }
+        right++;    
+        }
     return false;    
     }
 };
