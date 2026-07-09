d7b523a9e1bb59ba34e9fdbe1c45d2c347c3094e
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-09 08:56:37 +0530

    Time: 3 ms (80.23%), Space: 11.6 MB (63.39%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,14 +1,58 @@
-class Solution {
+// class Solution {
+// public:
+//     vector<int> findAnagrams(string s, string p) {
+//         vector<int>ans;
+//         int freq1[26]={0};
+//         int freq2[26]={0};
+//         for(char ch : p){
+//             freq1[ch-'a']++;
+//         }
+
+//         int left=0, right=0;
+//         while(right<s.size()){
+//             freq2[s[right]-'a']++;
+//             if(right-left+1==p.size() && equal(freq1, freq1+26, freq2)){
+//                 ans.push_back(left);
+//                 freq2[s[left]-'a']--;
+//                 left++;
+//             }
+
+//             if(right-left+1==p.size() && !equal(freq1, freq1+26, freq2)){
+//                 freq2[s[left]-'a']--;
+//                 left++;
+//             }
+//             right++;
+//         }
+//     return ans;    
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
 public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int>ans;
+    vector<int>findAnagrams(string s, string p){
+        if(p.size()>s.size()){
+            return {};
+        }
+
         int freq1[26]={0};
         int freq2[26]={0};
-        for(char ch : p){
+
+        for(char ch: p){
             freq1[ch-'a']++;
         }
 
         int left=0, right=0;
+        vector<int>ans;
         while(right<s.size()){
             freq2[s[right]-'a']++;
             if(right-left+1==p.size() && equal(freq1, freq1+26, freq2)){
@@ -17,11 +61,13 @@ public:
                 left++;
             }
 
-            if(right-left+1==p.size() && !equal(freq1, freq1+26, freq2)){
+            else if(right-left+1==p.size() && !equal(freq1, freq1+26, freq2)){
                 freq2[s[left]-'a']--;
                 left++;
             }
-            right++;
+        right++;    
+
+
         }
     return ans;    
     }
