b27074df3ea717606e2daac9a9f007e2c1638a0f
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-11 17:55:05 +0530

    Time: 4 ms (14.35%), Space: 17.2 MB (29.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,63 +1,122 @@
-class Solution {
+// class Solution {
+// public:
+//     int orangesRotting(vector<vector<int>>& grid) {
+//         int n= grid.size();
+//         int m= grid[0].size();
+//         int ans=0;
+
+//         vector<vector<bool>> vis(n, vector<bool>(m, false));
+//         queue<pair<pair<int, int>, int>>q;//((i,j), time)
+//         //pushing all sources in queue
+//         for(int i=0;i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(grid[i][j]==2){
+//                     q.push({{i,j}, 0});
+//                     vis[i][j]= true;
+//                 }
+//             }
+//         }
+
+//         //bfs
+//         while(q.size()!=0){
+//             int i= q.front().first.first;
+//             int j= q.front().first.second;
+//             int time= q.front().second;
+
+//             q.pop();
+//             ans= max(ans, time);
+
+//             if(i-1>=0 && !vis[i-1][j] && grid[i-1][j]==1){//top
+//                 q.push({{i-1, j}, time+1});
+//                 vis[i-1][j]= true;
+//             }
+
+//             if(j+1<m && !vis[i][j+1] && grid[i][j+1]==1){//right
+//                 q.push({{i, j+1}, time+1});
+//                 vis[i][j+1]= true;
+//             }
+
+//             if(i+1<n && !vis[i+1][j] && grid[i+1][j]==1){//bottom
+//                 q.push({{i+1, j}, time+1});
+//                 vis[i+1][j]= true;
+//             }
+
+//             if(j-1>=0 && !vis[i][j-1] && grid[i][j-1]==1){//left
+//                 q.push({{i, j-1}, time+1});
+//                 vis[i][j-1]= true;
+//             }
+
+//         }
+
+//     //check for fresh orange
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<m; j++){
+//             if(grid[i][j]==1 && !vis[i][j]){
+//                 return -1;
+//             }
+//         }
+//     }
+
+//     return ans;    
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
+    int orangesRotting(vector<vector<int>>& grid){
         int n= grid.size();
         int m= grid[0].size();
-        int ans=0;
-
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        queue<pair<pair<int, int>, int>>q;//((i,j), time)
-        //pushing all sources in queue
-        for(int i=0;i<n; i++){
+        vector<vector<int>>vis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>>q;
+        int forg=0;
+        for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j}, 0});
-                    vis[i][j]= true;
+                    q.push({{i, j},0});
+                    vis[i][j]=2;
+                }else if(grid[i][j]==1){
+                    forg++;
                 }
             }
         }
 
-        //bfs
-        while(q.size()!=0){
-            int i= q.front().first.first;
-            int j= q.front().first.second;
-            int time= q.front().second;
+        int dr[]= {-1, 0, 1, 0};
+        int dc[]= {0, 1, 0, -1};
+        int tm= 0;
 
+        while(!q.empty()){
+            int row= q.front().first.first;
+            int col= q.front().first.second;
+            int t= q.front().second;
+            tm= max(tm, t);
             q.pop();
-            ans= max(ans, time);
 
-            if(i-1>=0 && !vis[i-1][j] && grid[i-1][j]==1){//top
-                q.push({{i-1, j}, time+1});
-                vis[i-1][j]= true;
-            }
+            for(int i=0; i<4; i++){
+                int nrow= row+dr[i];
+                int ncol= col+dc[i];
 
-            if(j+1<m && !vis[i][j+1] && grid[i][j+1]==1){//right
-                q.push({{i, j+1}, time+1});
-                vis[i][j+1]= true;
-            }
-
-            if(i+1<n && !vis[i+1][j] && grid[i+1][j]==1){//bottom
-                q.push({{i+1, j}, time+1});
-                vis[i+1][j]= true;
-            }
-
-            if(j-1>=0 && !vis[i][j-1] && grid[i][j-1]==1){//left
-                q.push({{i, j-1}, time+1});
-                vis[i][j-1]= true;
-            }
-
-        }
-
-    //check for fresh orange
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(grid[i][j]==1 && !vis[i][j]){
-                return -1;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]!=2){
+                    q.push({{nrow, ncol}, t+1});
+                    vis[nrow][ncol]= 2;
+                    forg--;
+                }
             }
         }
-    }
-
-    return ans;    
+    if(forg!=0){
+        return -1;
+    }    
+    return tm;
+       
+        
     }
 };
-
