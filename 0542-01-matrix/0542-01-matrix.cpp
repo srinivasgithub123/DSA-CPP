f6abdcb2c47a7a0368719efec35f81332d81ee6e
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-11 18:58:59 +0530

    Time: 23 ms (40.43%), Space: 34.7 MB (87.02%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,120 @@
+// class Solution {
+// public:
+//     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+//         int n= mat.size();
+//         int m= mat[0].size();
+//         vector<vector<int>>dist(n, vector<int>(m, -1));
+//         queue<pair<int, int>>q;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(mat[i][j]==0){
+//                     dist[i][j]=0;
+//                     q.push({i, j});
+//                 }
+//             }
+//         }
+
+//         vector<int>dr= {1, -1, 0, 0};
+//         vector<int>dc= {0, 0, 1, -1};
+
+//         while(!q.empty()){
+//             auto[row, col]= q.front();
+//             q.pop();
+
+//             for(int i=0; i<4; i++){
+//                 int newr= row+dr[i];
+//                 int newc= col+dc[i];
+
+//                 if(newr>=0 && newc>=0 && newr<n && newc<m && dist[newr][newc]==-1){
+//                     dist[newr][newc]= dist[row][col]+1;
+//                     q.push({newr, newc});
+//                 }
+//             }
+//         }
+//         return dist;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> wallsMatrix(vector<vector<int>>& mat) {
+//         int n= mat.size();
+//         int m= mat[0].size();
+//         queue<pair<pair<int, int>, int>>q;
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 if(mat[i][j]==0){
+//                     q.push({{i,j}, 0});
+//                 }
+//             }
+//         }
+
+//         vector<int>dr= {-1, 0, 1, 0};
+//         vector<int>dc= {0, -1, 0, 1};
+//         while(!q.empty()){
+//             int row= q.front().first.first;
+//             int col= q.front().first.second;
+//             int dist= q.front().second;
+//             q.pop();
+
+//             for(int i=0; i<4; i++){
+//                 int newr=row+dr[i];
+//                 int newc=col+dc[i];
+
+//                 if(newr>=0 && newc>=0 && newc<m && newr<n && mat[newr][newc]==1e9){
+//                     mat[newr][newc]=dist+1;
+//                     q.push({{newr, newc}, dist+1});
+//                 }
+//             }
+//         }
+
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+class Solution{
+public:
+    vector<vector<int>>updateMatrix(vector<vector<int>>& mat){
+        int n= mat.size();
+        int m= mat[0].size();
+        vector<vector<int>>dist(n, vector<int>(m, -1));
+        queue<pair<int, int>>q;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j]==0){
+                    q.push({i, j});
+                    dist[i][j]= 0;
+                }
+            }
+        }
+        int dr[]= {-1, 0, 1, 0};
+        int dc[]={0, 1, 0, -1};
+
+        while(!q.empty()){
+            int row= q.front().first;
+            int col= q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                int nrow= row+dr[i];
+                int ncol= col+dc[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && dist[nrow][ncol]==-1){
+                    dist[nrow][ncol]= dist[row][col]+1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    return dist;    
+    }
+};
