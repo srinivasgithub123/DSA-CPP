2844c2b5a90f1d1452cb691999981b71c753bd97
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-18 10:31:47 +0530

    Time: 0 ms (100%), Space: 21.6 MB (5.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,73 +1,140 @@
-// class Solution {
+// // class Solution {
+// // public:
+// //     int findCircleNum(vector<vector<int>>& isConnected) {
+// //         int n=isConnected.size();
+// //         vector<int>visited(n,0);
+// //         vector<vector<int>>adj(n);
+// //         int prov=0;
+// //         for(int i=0; i<n; i++){
+// //             for(int j=0; j<n; j++){
+// //                 if(i!=j && isConnected[i][j]==1){
+// //                     adj[i].push_back(j);
+// //                 }
+// //             }
+// //         }
+        
+// //         for(int i = 0; i < n; i++) {
+// //             if(!visited[i]) {
+// //                 queue<int> q;
+// //                 q.push(i);
+// //                 visited[i] = true;
+// //                 prov++;
+
+// //                 while(!q.empty()) {
+// //                     int node = q.front(); q.pop();
+
+// //                     for(int neigh : adj[node]) {
+// //                         if(!visited[neigh]) {
+// //                             visited[neigh] = true;
+// //                             q.push(neigh);
+// //                     }
+// //                 }
+// //             }
+// //         }
+// //     }
+
+
+// //     return prov;    
+// //     }
+// // };
+
+// // class Solution {
+// // public:
+// //     int findCircleNum(vector<vector<int>>& isConnected) {
+// //         int n = isConnected.size();
+// //         vector<int> visited(n, 0);
+// //         int provinces = 0;
+
+// //         for(int i = 0; i < n; i++) {
+// //             if(!visited[i]) {
+// //                 provinces++;
+// //                 queue<int> q;
+// //                 q.push(i);
+// //                 visited[i] = 1;
+
+// //                 while(!q.empty()) {
+// //                     int city = q.front();
+// //                     q.pop();
+
+// //                     for(int j = 0; j < n; j++) {
+// //                         if(isConnected[city][j] == 1 && !visited[j]) {
+// //                             visited[j] = 1;
+// //                             q.push(j);
+// //                         }
+// //                     }
+// //                 }
+// //             }
+// //         }
+// //         return provinces;
+// //     }
+// // };
+
+
+
+
+
+
+
+
+
+// class Solution{
 // public:
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-//         int n=isConnected.size();
-//         vector<int>visited(n,0);
+
+//     // void dfs(vector<vector<int>>&adj, vector<int>& vis, int node){
+//     //     vis[node]=1;
+//     //     for(auto it: adj[node]){
+//     //         if(!vis[it])
+//     //         dfs(adj, vis, it);
+//     //     }
+//     // }
+
+//     void bfs(vector<vector<int>>&adj, vector<int>&vis, int node){
+//         vis[node]=1;
+//         queue<int>q;
+//         q.push(node);
+
+//         while(!q.empty()){
+//             int cnode= q.front();
+//             q.pop();
+
+//             for(auto it: adj[cnode]){
+//                 if(!vis[it]){
+//                     vis[it]=1;
+//                     q.push(it);
+//                 }
+//             }
+//         }
+
+//     }
+
+//     int findCircleNum(vector<vector<int>>& isConnected){
+//         int n= isConnected.size();
+//         vector<int>vis(n, 0);
 //         vector<vector<int>>adj(n);
-//         int prov=0;
 //         for(int i=0; i<n; i++){
 //             for(int j=0; j<n; j++){
 //                 if(i!=j && isConnected[i][j]==1){
 //                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
 //                 }
 //             }
 //         }
-        
-//         for(int i = 0; i < n; i++) {
-//             if(!visited[i]) {
-//                 queue<int> q;
-//                 q.push(i);
-//                 visited[i] = true;
-//                 prov++;
+//         int provincess=0;
 
-//                 while(!q.empty()) {
-//                     int node = q.front(); q.pop();
-
-//                     for(int neigh : adj[node]) {
-//                         if(!visited[neigh]) {
-//                             visited[neigh] = true;
-//                             q.push(neigh);
-//                     }
-//                 }
+//         for(int i=0; i<n; i++){
+//             if(vis[i]==0){
+//                 provincess++;
+//                 bfs(adj, vis, i);
 //             }
 //         }
-//     }
-
-
-//     return prov;    
+//     return provincess;    
 //     }
 // };
 
-// class Solution {
-// public:
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-//         int n = isConnected.size();
-//         vector<int> visited(n, 0);
-//         int provinces = 0;
 
-//         for(int i = 0; i < n; i++) {
-//             if(!visited[i]) {
-//                 provinces++;
-//                 queue<int> q;
-//                 q.push(i);
-//                 visited[i] = 1;
 
-//                 while(!q.empty()) {
-//                     int city = q.front();
-//                     q.pop();
 
-//                     for(int j = 0; j < n; j++) {
-//                         if(isConnected[city][j] == 1 && !visited[j]) {
-//                             visited[j] = 1;
-//                             q.push(j);
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return provinces;
-//     }
-// };
+
 
 
 
@@ -80,15 +147,7 @@
 class Solution{
 public:
 
-    // void dfs(vector<vector<int>>&adj, vector<int>& vis, int node){
-    //     vis[node]=1;
-    //     for(auto it: adj[node]){
-    //         if(!vis[it])
-    //         dfs(adj, vis, it);
-    //     }
-    // }
-
-    void bfs(vector<vector<int>>&adj, vector<int>&vis, int node){
+    void bfs(vector<vector<int>>& adj, vector<int>& vis, int node){
         vis[node]=1;
         queue<int>q;
         q.push(node);
@@ -104,13 +163,12 @@ public:
                 }
             }
         }
-
     }
-
+    
     int findCircleNum(vector<vector<int>>& isConnected){
-        int n= isConnected.size();
-        vector<int>vis(n, 0);
+        int n= isConnected.size(), provincess=0;
         vector<vector<int>>adj(n);
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 if(i!=j && isConnected[i][j]==1){
@@ -119,14 +177,15 @@ public:
                 }
             }
         }
-        int provincess=0;
 
+        vector<int>vis(n, 0);
         for(int i=0; i<n; i++){
-            if(vis[i]==0){
-                provincess++;
+            if(!vis[i]){
                 bfs(adj, vis, i);
+                provincess++;
             }
         }
-    return provincess;    
+        
+    return provincess;
     }
 };
