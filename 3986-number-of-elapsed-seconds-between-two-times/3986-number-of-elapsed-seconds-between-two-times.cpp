b0dec303eb5608bfe692771f068d60aadb0e977d
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-19 13:43:41 +0530

    Time: 0 ms (100%), Space: 10.4 MB (12.14%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.cpp
@@ -1,18 +1,50 @@
-class Solution {
+// class Solution {
+// public:
+
+//     int timeToSeconds(string time){
+//         int hh= stoi(time.substr(0, 2));
+//         int mm= stoi(time.substr(3, 2));
+//         int ss= stoi(time.substr(6, 2));
+//         int totalsec= (hh*3600)+(mm*60)+ss;
+//         return totalsec;
+//     }
+
+//     int secondsBetweenTimes(string startTime, string endTime) {
+//         int start= timeToSeconds(startTime);
+//         int end= timeToSeconds(endTime);
+
+//      return end-start;        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
 public:
 
     int timeToSeconds(string time){
+        
         int hh= stoi(time.substr(0, 2));
-        int mm= stoi(time.substr(3, 2));
-        int ss= stoi(time.substr(6, 2));
-        int totalsec= (hh*3600)+(mm*60)+ss;
-        return totalsec;
+        
+        int mm= stoi(time.substr(3,2));
+        
+        int ss=stoi(time.substr(6, 2));
+
+        return (hh*3600)+(mm*60)+ss;
     }
 
-    int secondsBetweenTimes(string startTime, string endTime) {
-        int start= timeToSeconds(startTime);
-        int end= timeToSeconds(endTime);
-
-     return end-start;        
+    int secondsBetweenTimes(string startTime, string endTime){
+        return timeToSeconds(endTime)-timeToSeconds(startTime);
     }
 };
