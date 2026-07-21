a197e2bd0554651687acc813624c0ee9c46c5685
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-21 10:46:30 +0530

    Time: 61 ms (50.29%), Space: 21.4 MB (29.72%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,31 +1,77 @@
-class Solution {
+// class Solution {
+// public:
+//     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+//         queue<pair<string, int>>q;
+//         q.push({beginWord, 1});
+//         unordered_set<string> st(wordList.begin(), wordList.end());
+//         st.erase(beginWord);
+
+//         while(!q.empty()){
+//             string word= q.front().first;
+//             int steps= q.front().second;
+//             q.pop();
+//             if(word==endWord){
+//                 return steps;
+//             }
+
+//             for(int i=0; i<word.size(); i++){
+//                 char original= word[i];
+//                 for(char ch='a';ch<='z'; ch++){
+//                     word[i]= ch;
+//                     if(st.count(word)){
+//                         st.erase(word);
+//                         q.push({word, steps+1});
+//                     }
+//                 }
+//                 word[i]= original;
+//             }
+//         }
+//         return 0;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    int ladderLength(string beginWord, string endWord, vector<string>&wordList){
         queue<pair<string, int>>q;
         q.push({beginWord, 1});
-        unordered_set<string> st(wordList.begin(), wordList.end());
+        unordered_set<string>st(wordList.begin(), wordList.end());
         st.erase(beginWord);
-
+        
         while(!q.empty()){
             string word= q.front().first;
             int steps= q.front().second;
             q.pop();
+
             if(word==endWord){
                 return steps;
             }
 
             for(int i=0; i<word.size(); i++){
                 char original= word[i];
-                for(char ch='a';ch<='z'; ch++){
+                for(char ch='a'; ch<='z'; ch++){
                     word[i]= ch;
-                    if(st.count(word)){
+                    if(st.find(word)!=st.end()){
                         st.erase(word);
                         q.push({word, steps+1});
                     }
                 }
-                word[i]= original;
+                word[i]=original;
             }
         }
-        return 0;
+    return 0;    
     }
 };
