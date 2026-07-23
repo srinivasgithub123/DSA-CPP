1503f588ce2de9eb580745e0d55902a271940843
srinivasgithub123 <srinivaskankala2004@gmail.com>
2026-07-23 09:32:12 +0530

    Time: 15 ms (72.07%), Space: 25.1 MB (62.19%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -50,42 +50,92 @@
 
 
 
-class Solution {
-public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
+// class Solution {
+// public:
+//     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+//         int n = grid.size();
         
-        // blocked start or end
-        if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
+//         // blocked start or end
+//         if (grid[0][0] == 1 || grid[n-1][n-1] == 1)
+//             return -1;
+
+//         queue<pair<int,int>> q;
+//         q.push({0, 0});
+//         grid[0][0] = 1;   // distance = 1 (also marks visited)
+
+//         vector<int> dr = {-1,-1,-1, 0, 0, 1, 1, 1};
+//         vector<int> dc = {-1, 0, 1,-1, 1,-1, 0, 1};
+
+//         while (!q.empty()) {
+//             auto [r, c] = q.front();
+//             q.pop();
+
+//             int dist = grid[r][c];
+
+//             if (r == n-1 && c == n-1)
+//                 return dist;
+
+//             for (int i = 0; i < 8; i++) {
+//                 int nr = r + dr[i];
+//                 int nc = c + dc[i];
+
+//                 if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0) {
+//                     grid[nr][nc] = dist + 1;
+//                     q.push({nr, nc});
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid){
+        int n= grid.size();
+        if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
+        }
+        vector<vector<int>>dist(n, vector<int>(n, 1e9));
+        queue<pair<int, int>>q;
+        dist[0][0]=1;
+        q.push({0,0});
+        int dr[] = {-1,-1,-1,0,0,1,1,1};
+        int dc[] = {-1,0,1,-1,1,-1,0,1};
 
-        queue<pair<int,int>> q;
-        q.push({0, 0});
-        grid[0][0] = 1;   // distance = 1 (also marks visited)
-
-        vector<int> dr = {-1,-1,-1, 0, 0, 1, 1, 1};
-        vector<int> dc = {-1, 0, 1,-1, 1,-1, 0, 1};
-
-        while (!q.empty()) {
-            auto [r, c] = q.front();
+        while(!q.empty()){
+            int row= q.front().first;
+            int col= q.front().second;
+            int dis=dist[row][col];
             q.pop();
 
-            int dist = grid[r][c];
+            for(int i=0; i<8; i++){
+                int nr= row+dr[i];
+                int nc= col+dc[i];
 
-            if (r == n-1 && c == n-1)
-                return dist;
-
-            for (int i = 0; i < 8; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if (nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0) {
-                    grid[nr][nc] = dist + 1;
+                if(nr>=0 && nc>=0 && nr<n && nc<n && grid[nr][nc]==0 && dis+1<dist[nr][nc]){
+                    dist[nr][nc]=dis+1;
                     q.push({nr, nc});
                 }
             }
         }
-        return -1;
-    }
+        if(dist[n-1][n-1]!=1e9){
+            return dist[n-1][n-1];
+        } 
+    return -1;       
+    }    
 };
 
